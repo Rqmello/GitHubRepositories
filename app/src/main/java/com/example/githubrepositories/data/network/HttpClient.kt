eeea0417c6b01a08f21c888e524fa79101f86107
Raquel <raquel.mello.2011@gmail.com>
2023-05-08 20:01:15 -0300

    Correção de formato baseUrl

--- app/src/main/java/com/example/githubrepositories/data/network/HttpClient.kt
+++ app/src/main/java/com/example/githubrepositories/data/network/HttpClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object HttpClient{
 
-    private val BASE_URL = "https://api.github.com/"
+    private val BASE_URL = "https://api.github.com"
 
     val retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
