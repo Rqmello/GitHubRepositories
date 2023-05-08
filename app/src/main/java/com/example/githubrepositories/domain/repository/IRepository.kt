38a3c79ae97a3e93d53a80550d3eff05076152ab
Raquel <raquel.mello.2011@gmail.com>
2023-05-08 20:05:20 -0300

    Criado a Interface Repository e Class Repository

---
+++ app/src/main/java/com/example/githubrepositories/domain/repository/IRepository.kt
@@ -0,0 +1,8 @@
+package com.example.githubrepositories.domain.repository
+
+import com.example.githubrepositories.data.remote.response.RepositoriesResponse
+
+interface IRepository {
+
+    suspend fun getAllRepositories(language: String, sort: String, page: Int): List<RepositoriesResponse>
+}
