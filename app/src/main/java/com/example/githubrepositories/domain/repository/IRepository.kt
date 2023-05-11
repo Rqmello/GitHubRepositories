353ab21de7d2125bb33eea2feb4e1fe024f4aaf8
Raquel <raquel.mello.2011@gmail.com>
2023-05-11 00:00:51 -0300

    Inclusão da função que chama o endpoint da lista de PullRequest por criador/repositório

--- app/src/main/java/com/example/githubrepositories/domain/repository/IRepository.kt
+++ app/src/main/java/com/example/githubrepositories/domain/repository/IRepository.kt
@@ -1,9 +1,12 @@
 package com.example.githubrepositories.domain.repository
 
 import com.example.githubrepositories.data.remote.response.RepositoriesResponse
+import com.example.githubrepositories.domain.model.PullRequestItemModel
 import com.example.githubrepositories.domain.model.RepositoriesModel
 
 interface IRepository {
 
     suspend fun getAllRepositories(language: String, sort: String, page: Int): RepositoriesModel
+
+    suspend fun getAllPullRepositories(criador: String, repositorio: String): PullRequestItemModel
 }
